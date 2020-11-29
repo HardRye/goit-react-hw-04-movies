b2f8f65e31c98afa225b4833c3c72c2b81ebb154
HardRain <nikolaimihail@gmail.com>
2020-11-30 00:21:04 +0200

    test

--- build/static/js/not-found-page.69e9db27.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[6],{85:function(e,t,a){"use strict";a.r(t);var n=a(0),r=a.n(n);t.default=function(){return r.a.createElement("h1",{style:{textAlign:"center"}},"Sorry, page not found",r.a.createElement("span",{role:"img","aria-label":"emoji"},"\ud83e\udd14"))}}}]);
-//# sourceMappingURL=not-found-page.69e9db27.chunk.js.map
